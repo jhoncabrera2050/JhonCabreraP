4c7646c020fb2897beb2d1f00ca4a193b825f11a
jhoncabrera <jhoncabrera2050@gmail.com>
2023-09-07 22:02:03 -0500

    actulizado

--- JhonCabrera/Camiones.xaml.cs
+++ JhonCabrera/Camiones.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,32 @@ namespace JhonCabrera
     /// </summary>
     public partial class Camiones : Window
     {
+        public class Camion
+        {
+            public string PesoMaximo { get; set; }
+            public string Placa { get; set; }
+            public string PesoVacio { get; set; }
+        }
+        private ObservableCollection<Camion> camionesList = new ObservableCollection<Camion>();
         public Camiones()
         {
             InitializeComponent();
+            lstCamiones.ItemsSource = camionesList;
         }
+
+        private void Agregar_Click(object sender, RoutedEventArgs e)
+        {
+            string pesoMaximo = txtPesoMaximo.Text;
+            string placa = txtPlaca.Text;
+            string pesoVacio = txtPesoVacio.Text;
+
+            Camion camion = new Camion { PesoMaximo = pesoMaximo, Placa = placa, PesoVacio = pesoVacio };
+            camionesList.Add(camion);
+
+            txtPesoMaximo.Clear();
+            txtPlaca.Clear();
+            txtPesoVacio.Clear();
+        }
+
     }
 }
